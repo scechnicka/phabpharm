2c74a0cdaf360fe4b89af4c183c291d291ea9f3c
scechnicka <sc7718@ic.ac.uk>
2020-12-21 12:15:45 +0000

    Icons

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -25,7 +25,49 @@ public void doPost(HttpServletRequest req, HttpServletResponse resp) throws Serv
         if ((name.equals("Sarah")) && (password.equals("love")))
         {
                 resp.setContentType("text/html");
-                resp.getWriter().write("Hello World");
+                resp.getWriter().write("<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                        "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">\n" +
+                        "<style>\n" +
+                        "body {margin:0;}\n" +
+                        "\n" +
+                        ".icon-bar {\n" +
+                        "  width: 100%;\n" +
+                        "  background-color: #555;\n" +
+                        "  overflow: auto;\n" +
+                        "}\n" +
+                        "\n" +
+                        ".icon-bar a {\n" +
+                        "  float: left;\n" +
+                        "  width: 20%;\n" +
+                        "  text-align: center;\n" +
+                        "  padding: 12px 0;\n" +
+                        "  transition: all 0.3s ease;\n" +
+                        "  color: white;\n" +
+                        "  font-size: 36px;\n" +
+                        "}\n" +
+                        "\n" +
+                        ".icon-bar a:hover {\n" +
+                        "  background-color: #000;\n" +
+                        "}\n" +
+                        "\n" +
+                        ".active {\n" +
+                        "  background-color: #4CAF50;\n" +
+                        "}\n" +
+                        "</style>\n" +
+                        "<body>\n" +
+                        "\n" +
+                        "<div class=\"icon-bar\">\n" +
+                        "  <a class=\"active\" href=\"#\"><i class=\"fa fa-home\"></i></a> \n" +
+                        "  <a href=\"#\"><i class=\"fa fa-search\"></i></a> \n" +
+                        "  <a href=\"#\"><i class=\"fa fa-envelope\"></i></a> \n" +
+                        "  <a href=\"#\"><i class=\"fa fa-globe\"></i></a>\n" +
+                        "  <a href=\"#\"><i class=\"fa fa-trash\"></i></a> \n" +
+                        "</div>\n" +
+                        "\n" +
+                        "</body>\n" +
+                        "</html> \n");
         }
         else
         {
