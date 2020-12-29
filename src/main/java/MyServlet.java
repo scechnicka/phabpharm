2ac094de43d6f1a6a24f95cbf4781e0ba3777c0f
scechnicka <sc7718@ic.ac.uk>
2020-12-29 14:57:12 +0000

    'changing welcome page'

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
-@WebServlet ("/")
+@WebServlet ("/welcome")
 public class MyServlet extends HttpServlet {
     int count =0;
     @Override
