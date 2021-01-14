b8446c36fefac0901bd963acf5362a44b830249b
scechnicka <sc7718@ic.ac.uk>
2021-01-14 18:46:07 +0000

    "DB connection"

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -5,12 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 
 
 @WebServlet (name = "MyServlet", urlPatterns = {"/MyServlet"})
