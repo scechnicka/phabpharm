20dcc68c0249df074801668a3e08a3195071a3fe
scechnicka <sc7718@ic.ac.uk>
2021-01-15 12:52:12 +0000

    "print one product"

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -110,10 +110,10 @@ public void doPost(HttpServletRequest req, HttpServletResponse resp) throws Serv
         if ((cdb.lastName.contains(name)) && (cdb.password.contains(password)))
         {
                 req.setAttribute("product_list",products_bis);
-                req.getRequestDispatcher("Desktop.jsp").include(req, resp);
-                //resp.setContentType("text/html");
-                //PrintWriter out4=resp.getWriter();
-                //out4.println(products_bis.get(4).getAmount());
+                //req.getRequestDispatcher("Desktop.jsp").include(req, resp);
+                resp.setContentType("text/html");
+                PrintWriter out4=resp.getWriter();
+                out4.println(products_bis.get(0).id);
 
         }
         else
