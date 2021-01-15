1f2f7358ffaa83d334cc7d938e3b238fd6a942d4
hannahlangart <hannah.langart18@imperial.ac.uk>
2021-01-15 11:19:51 +0100

    Mockito and testing Servlet

---
+++ src/test/java/TestServlet.java
@@ -0,0 +1,43 @@
+/*
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+
+public class TestServlet {
+
+    @Mock
+    HttpServletRequest request;
+    @Mock
+    HttpServletResponse response;
+
+    @Before
+    public void setUp() throws Exception{
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void testDoGet() throws IOException, ServletException {
+        StringWriter stringWriter=new StringWriter();
+        PrintWriter printWriter=new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(printWriter);
+        when(request.getServletPath()).thenReturn("login.jsp");
+        MyServlet myServlet=new MyServlet();
+        myServlet.doGet(request,response);
+        String output=stringWriter.getBuffer().toString();
+        Assert.assertThat(output,is(equalTo("OK")));
+    }
+}
+
+*/
